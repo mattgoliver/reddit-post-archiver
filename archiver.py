d1abed64228a59abadfb55b9cd7775ef3716b18f
mattgoliver <matt.oliver.2004@gmail.com>
2023-10-15 14:24:13 -0400

    Now able to retrieve saved posts and loop through them.

--- archiver.py
+++ archiver.py
@@ -9,6 +9,7 @@ import json  # Allows access to the .json login information file
 import praw  # Allows access to Reddit's API
 import sqlite3  # Allows for database management
 import os  # Allows for checking if database exists
+import uuid  # Allows for generation of unique file names
 
 
 def read_secrets(filename):
@@ -27,61 +28,72 @@ def read_secrets(filename):
         print(f"ERROR: No .json file found for login information with the name '{filename}'")
 
 
-def archive_saved_posts(user, upvote=False):
-    """Retrieves all saved posts from a user's profile.
+def archive_saved_posts(upvote=False, limit=None, log=False):
+    """Retrieves all saved posts from user's profile.
 
     Args:
-        user (str): User to get saved posts from.
         upvote (bool): Upvote post once archived, default=False
+        limit (int): Amount of posts to retrieve, default=1000, max=1000
+        log (bool): Print log messages, default = False
 
     Returns:
         dict: Dictionary containing post information.
     """
-    saved_posts = []
-
-    for post in saved_posts:  # Loop through user's saved posts
-        if access.check_duplicate(post.id):  # Skip to next post if duplicate
+    for post in reddit.user.me().saved(limit=limit):
+        if str(type(post)) == "<class 'praw.models.reddit.comment.Comment'>":  # Ignore saved comments
             continue
 
-        file_name = random.name()  # Create a random name to save the content as
+        file_name = download_post(post, log)
 
-        if download_post(post.url, file_name):  # Download post content, if download successful then archive
-            archive(post, file_name)  # Archive post content
-
-        if upvote:  # Upvote after archive if enabled
-            post.upvote()
+        archive(post, "test", log)
 
 
-def download_post(url, file_name):
+def download_post(post, log=False):
     """Downloads post at given url.
 
     Args:
-          url (str): URL of post to download.
-          file_name (str): Name to save file as.
+          post: Post to download.
+          log (bool): Print log messages, default = False
 
     Returns:
-         bool: Download success
+         tup: File name
     """
-    pass
+    file_name = ""
+
+    post_exists = True
+    while post_exists is True:
+        file_name = str(uuid.uuid4().hex) + ".txt"
+
+        if not os.path.isfile("./media/" + file_name):  # Check if file name already exists
+            post_exists = False
+            if log:
+                print(f"DOWNLOAD: '{file_name}' is unique.")
+        else:
+            if log:
+                print(f"DOWNLOAD: '{file_name}' already exists, regenerating.")
+
+    return file_name
 
 
-def archive(post, file_name):
+def archive(post, file_name, log=False):
     """Archives the post at the given url.
 
     Information archived: ID, title, author, content (text & media), subreddit, url, date, amount of upvotes
 
     Args:
-        post (dict): Post to archive.
+        post: Post to archive.
         file_name: Name of downloaded file to associate post information with.
+        log (bool): Print log messages, default = False
     """
-    pass
+    print(f"ARCHIVE: NOT FINISHED - Post title: {post.title}")
 
 
-def initialize_database_connection(database_name):
-    """Connects to the archive database, creates one if it doesn't exist
+def initialize_database_connection(database_name, log=False):
+    """Connects to the archive database, creates one if it doesn't exist.
 
     Args:
         database_name (str): Name of database to connect to.
+        log (bool): Print log messages, default = False
 
     Returns:
         tup: Database connection and cursor
@@ -89,21 +101,31 @@ def initialize_database_connection(database_name):
     if os.path.isfile(database_name):
         local_connection = sqlite3.connect(database_name)
         local_cursor = local_connection.cursor()
-        print(f'Database exists. Successfully connected to {database_name}')
+        if log:
+            print(f'SQLLite3: Database exists. Successfully connected to {database_name}')
 
     else:
-        print(f'Database does not exist. Creating {database_name}...')
+        if log:
+            print(f'SQLLite3: Database does not exist. Creating {database_name}...')
         local_connection = sqlite3.connect(database_name)
         local_cursor = local_connection.cursor()
         local_cursor.execute("create table posts (file_name text, id text, title text, author text, content_text text, "
                              "subreddit text, url text, upvotes integer, time integer)")
-        print(f"Database successfully created.")
+        if log:
+            print(f"SQLLite3: Database successfully created.")
 
     return local_connection, local_cursor
 
 
-if __name__ == "__main__":
-    # Initialize Reddit instance
+def initialize_reddit_connection(log=False):
+    """Connect to Reddit API via PRAW.
+
+    Args:
+        log (bool): Print log messages, default = False
+
+    Returns:
+        obj: Authenticated Reddit instance
+    """
     secrets = read_secrets("secrets.json")
     reddit = praw.Reddit(
         client_id=secrets["client_id"],
@@ -112,7 +134,21 @@ if __name__ == "__main__":
         user_agent=secrets["user_agent"],
         username=secrets["username"]
     )
-    reddit.read_only = True
+    if log:
+        print(f"PRAW: Successfully authenticated as user: {reddit.user.me()}")
+
+    return reddit
+
+
+if __name__ == "__main__":
+    # Initialize Reddit instance
+    reddit = initialize_reddit_connection(log=True)
 
     # Initialize database connection
-    connection, cursor = initialize_database_connection("reddit-post-archive.db")
+    # connection, cursor = initialize_database_connection("reddit-post-archive.db", log=True)
+
+    # archive_saved_posts(limit=5)
+
+    # Close database connection
+    # connection.close()
+    # print("SQLLite3: Database successfully closed.")
